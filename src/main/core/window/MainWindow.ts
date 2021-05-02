2a741780655bfaea22ab268632770796aa7f116c
kohei.hujita <point.out92@gmail.com>
2021-05-02 23:22:21 +0900

    クラス調整

--- src/main/core/window/MainWindow.ts
+++ src/main/core/window/MainWindow.ts
@@ -13,32 +13,44 @@ export class MainWindow extends BaseWindow {
     super(WINDOW_OPTIONS.main);
     this.setUsingHtmlName('index');
 
-    this.window?.on('ready-to-show', () => {
-      const connectInfo = store.get(storeKeys.CONNECT_INFO) as IDatabaseSetting;
-
-      if (connectInfo) {
-        const mysql = new MySQL(connectInfo);
-
-        mysql.connection.then(async () => {
-          const info = await mysql.collectInfo(connectInfo);
-          this.window?.webContents.send(ipcKeys.DBINFO, info);
-        });
-      } else {
-        this.settings = SettingWindow.create();
-        this.settings.window?.setParentWindow(this.window);
-      }
-    });
-
-    this.window?.on('closed', () => {
-      store.delete(storeKeys.CONNECT_INFO);
-
-      if (this.settings) {
-        this.settings.window?.close();
-      }
-    });
+    this.window?.on('ready-to-show', this.readyToShow.bind(this));
+    this.window?.on('closed', this.closed.bind(this));
   }
 
+  /**
+   * ウィンドウ作成
+   */
   static create(): BaseWindow {
     return new MainWindow();
+  }
+
+  /**
+   * ウィンドウの準備完了時に処理
+   */
+  readyToShow() {
+    const connectInfo = store.get(storeKeys.CONNECT_INFO) as IDatabaseSetting;
+
+    if (connectInfo) {
+      const mysql = new MySQL(connectInfo);
+
+      mysql.connection.then(async () => {
+        const info = await mysql.collectInfo(connectInfo);
+        this.window?.webContents.send(ipcKeys.DBINFO, info);
+      });
+    } else {
+      this.settings = SettingWindow.create();
+      this.settings.window?.setParentWindow(this.window);
+    }
+  }
+
+  /**
+   * ウィンドウがclosed時に処理
+   */
+  closed() {
+    store.delete(storeKeys.CONNECT_INFO);
+
+    if (this.settings) {
+      this.settings.window?.close();
+    }
   }
 }
