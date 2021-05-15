0f31fa28768ec28bfb118b0382dc8a7d6ea2ce53
kohei.hujita <point.out92@gmail.com>
2021-05-15 17:08:49 +0900

    separete database list to components

--- src/renderer/components/main/App.tsx
+++ src/renderer/components/main/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { ipcKeys } from '../../../common/ipcKeys';
-import { UtilFunc } from '../../utils/UtilFunc';
+import DatabaseList from './DatabaseList';
 import TitleBar from './TitleBar';
 
 interface AppState {
@@ -10,37 +9,12 @@ interface AppState {
 }
 
 class App extends React.Component<{}, AppState> {
-  tableRefs: React.RefObject<HTMLUListElement>[] = [];
-
   constructor(props: {}) {
     super(props);
 
     this.state = {
       mode: 'dark'
     };
-
-    ipcRenderer.on(ipcKeys.DBINFO, this.setTableRefs.bind(this));
-  }
-
-  /**
-   * データベース名を表示する要素
-   */
-  get databaseElements(): JSX.Element[] | undefined {
-    return this.state.dbinfo?.schemataList.map((schemata, i) => {
-      return (
-        <li className="database" key={i}>
-          <div
-            className="name"
-            data-database={schemata.name}
-            onClick={this.clickDatabaseName.bind(this)}
-          >
-            <div className="icon"><i className="fas fa-database"></i></div>
-            <div>{schemata.name}{/*: {schemata.collation}*/}</div>
-          </div>
-          <ul className="table-list" id={`database_${i}`} ref={this.tableRefs[i]}></ul>
-        </li>
-      );
-    });
   }
 
   /**
@@ -48,7 +22,6 @@ class App extends React.Component<{}, AppState> {
    */
   componentDidMount() {
     ipcRenderer.on(ipcKeys.DBINFO, this.dbinfoHandler.bind(this));
-    ipcRenderer.on(ipcKeys.SHOW_TABLES, this.showTables.bind(this));
   }
 
   /**
@@ -58,60 +31,6 @@ class App extends React.Component<{}, AppState> {
    */
   dbinfoHandler(e: Electron.IpcRendererEvent, dbinfo: IDatabaseInfoCollection | undefined) {
     this.setState({ dbinfo });
-  }
-
-  /**
-   * データベース名を取得し送信
-   * @param e
-   */
-  clickDatabaseName(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    const parentElement = e.currentTarget.parentElement;
-    const tableListElement = UtilFunc.getChildrenByClass(parentElement, 'table-list').shift();
-
-    const props: IDatabaseProps = {
-      id: tableListElement?.id as string,
-      name: e.currentTarget.dataset.database as string,
-    };
-
-    tableListElement?.classList.toggle('open');
-
-    if (tableListElement?.children.length === 0) {
-      ipcRenderer.send(ipcKeys.SEND_DB, props);
-    }
-  }
-
-  /**
-   * テーブル名を表示する
-   * @param e
-   * @param tableNames
-   */
-  showTables(e: Electron.IpcRendererEvent, props: { id: string; tableNames: string[] | undefined }) {
-    const target = this.tableRefs.find((ref) => {
-      return ref.current?.id === props.id;
-    })?.current;
-
-    if (target?.children.length === 0) {
-      const tableitems = props.tableNames?.map((table, i) => {
-        return (
-          <li key={i}><i className="fas fa-table"></i>{table}</li>
-        );
-      });
-
-      ReactDOM.render(<>{tableitems}</>, target);
-    }
-  }
-
-  /**
-   * テーブル名用のRefをセット
-   * @param e
-   * @param dbinfo
-   */
-  setTableRefs(e: Electron.IpcRendererEvent, dbinfo: IDatabaseInfoCollection | undefined) {
-    if (dbinfo && this.tableRefs.length === 0) {
-      for (let i = 0; i < dbinfo.schemataList.length; i++) {
-        this.tableRefs.push(React.createRef<HTMLUListElement>());
-      }
-    }
   }
 
   /**
@@ -127,7 +46,7 @@ class App extends React.Component<{}, AppState> {
       <>
         <TitleBar color="#c6cbd1" bgColor="#1e2226" />
         <main className={this.state.mode}>
-          <ul className="database-list">{this.databaseElements}</ul>
+          <DatabaseList dbinfo={this.state.dbinfo} />
         </main>
       </>
     );
---
+++ src/renderer/components/main/DatabaseList.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ipcKeys } from "../../../common/ipcKeys";
+import { UtilFunc } from "../../utils/UtilFunc";
+
+interface DatabaseListProps {
+  dbinfo?: IDatabaseInfoCollection;
+}
+
+class DatabaseList extends React.Component<DatabaseListProps> {
+  tableRefs: React.RefObject<HTMLUListElement>[] = [];
+
+  constructor(props: DatabaseListProps) {
+    super(props);
+    ipcRenderer.on(ipcKeys.DBINFO, this.setTableRefs.bind(this));
+  }
+
+  /**
+   * データベース名を表示する要素
+   */
+  get databaseElements(): JSX.Element[] | undefined {
+    return this.props.dbinfo?.schemataList.map((schemata, i) => {
+      return (
+        <li className="database" key={i}>
+          <div
+            className="name"
+            data-database={schemata.name}
+            onClick={this.clickDatabaseName.bind(this)}
+          >
+            <div className="icon"><i className="fas fa-database"></i></div>
+            <div>{schemata.name}{/*: {schemata.collation}*/}</div>
+          </div>
+          <ul className="table-list" id={`database_${i}`} ref={this.tableRefs[i]}></ul>
+        </li>
+      );
+    });
+  }
+
+  /**
+   * コンポネントがマウントされた後に実行
+   */
+  componentDidMount() {
+    ipcRenderer.on(ipcKeys.SHOW_TABLES, this.showTables.bind(this));
+  }
+
+  /**
+  * データベース名を取得し送信
+  * @param e
+  */
+  clickDatabaseName(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const parentElement = e.currentTarget.parentElement;
+    const tableListElement = UtilFunc.getChildrenByClass(parentElement, 'table-list').shift();
+
+    const props: IDatabaseProps = {
+      id: tableListElement?.id as string,
+      name: e.currentTarget.dataset.database as string,
+    };
+
+    tableListElement?.classList.toggle('open');
+
+    if (tableListElement?.children.length === 0) {
+      ipcRenderer.send(ipcKeys.SEND_DB, props);
+    }
+  }
+
+  /**
+   * テーブル名を表示する
+   * @param e
+   * @param tableNames
+   */
+  showTables(e: Electron.IpcRendererEvent, props: { id: string; tableNames: string[] | undefined }) {
+    const target = this.tableRefs.find((ref) => {
+      return ref.current?.id === props.id;
+    })?.current;
+
+    if (target?.children.length === 0) {
+      const tableitems = props.tableNames?.map((table, i) => {
+        return (
+          <li key={i}><i className="fas fa-table"></i>{table}</li>
+        );
+      });
+
+      ReactDOM.render(<>{tableitems}</>, target);
+    }
+  }
+
+  /**
+   * テーブル名用のRefをセット
+   * @param e
+   * @param dbinfo
+   */
+  setTableRefs(e: Electron.IpcRendererEvent, dbinfo: IDatabaseInfoCollection | undefined) {
+    if (dbinfo && this.tableRefs.length === 0) {
+      for (let i = 0; i < dbinfo.schemataList.length; i++) {
+        this.tableRefs.push(React.createRef<HTMLUListElement>());
+      }
+    }
+  }
+
+  /**
+   * レンダリング
+   * @returns
+   */
+  render() {
+    return (
+      <ul className="database-list">{this.databaseElements}</ul>
+    );
+  }
+}
+
+export default DatabaseList;
